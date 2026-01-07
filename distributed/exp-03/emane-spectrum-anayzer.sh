af30bb973f9aeb68aa6380074dd1bda640ff1c62
Joseph Giovatto <jgiovatto@adjacentlink.com>
2026-01-07 16:13:56 -0500

    added exp-04 for router to multiple radios

--- distributed/exp-03/emane-spectrum-anayzer.sh
+++ distributed/exp-03/emane-spectrum-anayzer.sh
@@ -9,10 +9,11 @@ then
 fi
 
 emane-spectrum-analyzer \
-    $1:8883 \
     --hz-min 2390000000 \
     --hz-max 2410000000 \
     --with-waveforms \
     --subid-name 1,radio-1 \
-    --subid-name 2,radio-2
+    --subid-name 2,radio-2 \
+    $1:8883 \
+    -174
 
