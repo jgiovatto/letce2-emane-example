f159f12b2f2784d9d4776200abd6157b491bc704
Joseph Giovatto <jgiovatto@adjacentlink.com>
2026-02-03 14:45:54 -0500

    fixed exp-02 wlans added heal and break scripts

--- exp-01/break_wan4.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -
-  
-DEV=letce0
-
-emaneevent-pathloss 13  200 -t 14 -t 15 -t 16 -i letce0
-emaneevent-pathloss 14  200 -t 13 -t 15 -t 16 -i letce0
-emaneevent-pathloss 15  200 -t 13 -t 14 -t 16 -i letce0
-emaneevent-pathloss 16  200 -t 13 -t 14 -t 15 -i letce0
-
--- exp-01/heal_wan4.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -
-  
-DEV=letce0
-
-emaneevent-pathloss 13  80 -t 14 -t 15 -t 16 -i letce0
-emaneevent-pathloss 14  80 -t 13 -t 15 -t 16 -i letce0
-emaneevent-pathloss 15  80 -t 13 -t 14 -t 16 -i letce0
-emaneevent-pathloss 16  80 -t 13 -t 14 -t 15 -i letce0
-
